1bbeed16df4e24c0a8cfaaeb029fd421f2083ce7
mrgiddz <{ID}+{username}@users.noreply.github.com>
2023-03-10 01:18:05 +0100

    cloudinary and postgres added

--- server/config/database.js
+++ server/config/database.js
@@ -1,10 +1,19 @@
 const path = require('path');
 
+
+//postgres://admin:PKFlwTNqaTG9UQtsabc7GU6WqZaj1Kpd@dpg-cg55keseoogsv95evc90-a.oregon-postgres.render.com/maindb_n6wp
+
 module.exports = ({ env }) => ({
   connection: {
-    client: 'sqlite',
+    client: 'postgres',
     connection: {
-      filename: path.join(__dirname, '..', env('DATABASE_FILENAME', '.tmp/data.db')),
+      // filename: path.join(__dirname, '..', env('DATABASE_FILENAME', '.tmp/data.db')),
+      host: env('DATABASE_HOST'),
+      port: env.int('DATABASE_PORT'),
+      database: env('DATABASE_NAME'),
+      user: env('DATABASE_USERNAME'),
+      password: env('DATABASE_PASSWORD'),
+      ssl: env.bool('DATABASE_SSL', true)
     },
     useNullAsDefault: true,
   },
